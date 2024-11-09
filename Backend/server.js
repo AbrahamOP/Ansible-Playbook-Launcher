d7c4e8fc0e7e876ceb66f0891089743d7c6106c6
Antoine .P <antoine.p22@outlook.com>
2024-11-09 14:30:28 +0100

    new

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,9 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const session = require('express-session');
-const sqlite3 = require('sqlite3').verbose();
-const bcrypt = require('bcrypt');
 
 const app = express();
 const playbooksDirectory = path.join('/playbooks'); // Dossier monté avec Docker pour les playbooks
-const db = new sqlite3.Database(':memory:'); // Utiliser une base de données en mémoire pour l'exemple
 
 app.use(express.json());
 app.use(session({
@@ -17,14 +14,6 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false } // Mettre à true en production avec HTTPS
 }));
-
-// Initialiser la base de données
-db.serialize(() => {
-  db.run("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
-  const saltRounds = 10;
-  const passwordHash = bcrypt.hashSync('password', saltRounds);
-  db.run("INSERT INTO users (username, password) VALUES (?, ?)", ['admin', passwordHash]);
-});
 
 // Servir les fichiers statiques depuis le dossier "frontend"
 app.use(express.static(path.join(__dirname, 'frontend')));
@@ -88,37 +77,6 @@ app.post('/api/execute', (req, res) => {
     fs.unlinkSync(hostsFilePath);
     console.log(`Fichier hosts supprimé de : ${hostsFilePath}`);
   });
-});
-
-// Route pour la connexion
-app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  db.get("SELECT * FROM users WHERE username = ?", [username], (err, user) => {
-    if (err) {
-      console.error('Erreur lors de la recherche de l\'utilisateur:', err);
-      res.status(500).json({ success: false });
-    } else if (user && bcrypt.compareSync(password, user.password)) {
-      req.session.user = { username };
-      res.json({ success: true });
-    } else {
-      res.json({ success: false });
-    }
-  });
-});
-
-// Route pour la déconnexion
-app.post('/api/logout', (req, res) => {
-  req.session.destroy();
-  res.json({ success: true });
-});
-
-// Route pour vérifier l'authentification
-app.get('/api/check-auth', (req, res) => {
-  if (req.session.user) {
-    res.json({ authenticated: true });
-  } else {
-    res.json({ authenticated: false });
-  }
 });
 
 // Rediriger les requêtes non capturées vers index.html
